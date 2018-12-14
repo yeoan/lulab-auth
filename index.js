a7909915c2c6b49789e154bd9d17e737b5bd7271
蔣曜安 <yeoan11317@gmail.com>
2018-12-14 15:08:19 +0800

    add express session passportjs

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 var express = require("express")
 var app = new express()
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 var path = require('path')
 var pg = require('pg')
 var config = require('./config')
+var session = require('express-session')
+const bcrypt = require('bcrypt')
+var passport = require('passport')
+var Strategy = require('passport-local').Strategy;
+var pgSession = require('connect-pg-simple')(session);
+var db = require('./db');
 
-// var QueryGetPlans = function(callback) {
-//     var client = new pg.Client(config.db);
-//     client.connect(err => {
-//         if (err) throw err;
-//         else {
-//           console.log('good c')
-//         }
-//     });
-//
-// }
+const saltRounds = 10;
 
-//QueryGetPlans();
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+  passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 app.use(express.static(__dirname + "/public"));
 
@@ -28,17 +46,74 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(require('cookie-parser')());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  store: new pgSession({
+    pg : pg,                                  // Use global pg-module
+    conString : 'http://127.0.0.1:5432', // Connect using something else than default DATABASE_URL env variable
+    tableName : 'session'               // Use another table-name than the default "session" one
+  }),
+  secret: 'keyboard cat',
+  resave: false, saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.get("/register", function(req, resp, next) {
+    resp.redirect("./register.html");
+});
 
+//'INSERT INTO "public"."users"("username", "email", "password") VALUES('+'\''+uname+'\',\''+email+'\',\''+pass+'\');'
 app.post("/register", function(req, resp, next) {
-    console.log(req.body.username)
+  var client = new pg.Client(config.db);
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  // Store hash in your password DB.
+  client.connect(err => {
+        if (err) {
+          resp.send({result:'conn failure'})
+        }
+        else {
+            client.query('INSERT INTO "public"."users"("username", "email", "password") VALUES('+'\''+req.body.username+'\',\''+req.body.email+'\',\''+hash+'\');')
+                .then(res => {
+                  console.log(res)
+                  resp.send({result:'regis success'})
+                })
+                .catch(err => {
+                    console.log(err);
+                    resp.send({result:'regis failure'})
+                });
+        }
+    });
+  });
 });
 
-// Not found page
-app.get("*", function(req, resp, next) {
-    resp.end("Not found page");
+app.get("/login", function(req, resp, next) {
+  if(req.user){
+    resp.redirect('/main.html')
+  }else{
+    resp.redirect('/login.html')
+  }
 });
+
+app.post("/login", passport.authenticate('local', { failureRedirect: '/login' }),function(req, resp, next) {
+    resp.redirect('/main.html')
+});
+
+function ensureAuthenticated(req, res, next){
+  if(req.user){
+    return next();
+  } else {
+    res.redirect('/login.html')
+  }
+}
+
+app.get("/main",ensureAuthenticated,function(req, resp, next) {
+    resp.redirect('/main'+req.user.username)
+});
+
 
 //Start Server
 app.listen(3000, function(err) {
--- public/app.js
+++ public/app.js
@@ -1,16 +1,30 @@
 
 
 
+function handleLogin(){
+  let email = document.getElementById('Login_Element_for_Email_field').value;
+  let password = document.getElementById('Login_Element_for_Password_field').value;
+  axios.post('http://127.0.0.1:3000/login', {
+    email: email,
+    password: password,
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+    alert('check your internet connection')
+  });
+}
+
+
+
 function handleSignUp(){
   let email = document.getElementById('Enter_the_Form_Element_for_Email_field').value;
   let password = document.getElementById('Enter_the_Form_Element_for_Password_field').value;
   let confirmPassword = document.getElementById('Enter_the_Form_Element_for_Password_check_field').value;
   let username = document.getElementById('Enter_the_Form_Element_for_Username_field').value;
-  let constraints = {
-  confirmPassword: {
-    equality: "password"
-  }
-};
+
   if(password===confirmPassword){
     signUp(username,email,password,confirmPassword);
   }else{
@@ -31,8 +45,19 @@ function signUp(uname,email,pass,pass2){
   })
   .then(function (response) {
     console.log(response);
+    if(response.data.result=='regis success'){
+      window.location.href = "http://127.0.0.1:3000/login"
+      alert('register successfully')
+    }
+    else if(response.data.result=='regis failure'){
+      alert('register falurely')
+    }
+    else if(response.data.result=='conn failure'){
+      alert('server error')
+    }
   })
   .catch(function (error) {
     console.log(error);
+    alert('check your internet connection')
   });
 }
